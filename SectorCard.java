fc9ab770a2fc47a42cfd9f8c6c3878e45a67fd65
Speedy-yjo <anaellle.melo@gmail.com>
2024-11-28 14:40:48 +0100

    Add files via upload

---
+++ SectorCard.java
@@ -0,0 +1,176 @@
+package pocket_imperium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SectorCard {
+    private int anchorX;
+    private int anchorY;
+    private String type; //MiddleExteriorCard ou CentralCard ou ExteriorCard
+    private String name;
+    private List<Hex> hexes= new ArrayList<>();
+	private boolean estTriPrime=false;
+	
+    /**
+     * La fonction permet de créer une carte de secteur
+     * @param anchorX correspond à la position en x (ligne) de la carte dans le plateau
+     * @param anchorY correspond à la position en y (colonne) de la carte dans le plateau 
+     * @param type correspond au type de carte ("Central", "MiddleExterior", "Exterior")
+     */
+	public SectorCard(String type) { //	public SectorCard(String name, String type) {
+        this.type = type;
+        this.anchorX = 0;
+        this.anchorY = 0;
+        this.name = "";
+        
+        this.hexes = new ArrayList<>();
+        if (type.equals("MiddleExteriorCard")) {
+            createMiddleExteriorCard();
+        } else if (type.equals("CentralCard")) {
+        	this.name = "TriPrime";
+            createCentralCard();
+        } else if (type.equals("ExteriorCard")) {
+            createExteriorCard();
+        } else {
+            throw new IllegalArgumentException("Type de carte inconnu : " + type);
+        }
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setPlacement(int x, int y) {
+    	this.anchorX=x;
+    	this.anchorY=y;
+    	if (x==0 && y==0) {
+    		this.name="TopLeft";
+    	}else if (x==0 && y==1) {
+    		this.name="TopMiddle";
+    	}else if (x==0 && y==2) {
+    		this.name="TopRight";
+    	}else if (x==1 && y==0) {
+    		this.name="MiddleLeft";
+    	}else if (x==1 && y==2) {
+    		this.name="MiddleRight";
+    	}else if (x==2 && y==0) {
+    		this.name="BottomLeft";
+    	}else if (x==2 && y==0) {
+    		this.name="BottomMiddle";
+    	}else if (x==2 && y==0) {
+    		this.name="BottomRight";
+    	}else {
+    		throw new IllegalArgumentException("Emplacement de carte impossible : x=" + x + "y=" + y);
+    	}
+    }
+    
+    public List<Hex> getHexes() {
+        return hexes;
+    }
+
+    public void addHexInSector(int relativeX, int relativeY, String typeH) {
+    	if (typeH=="TriPrime") {
+    		this.hexes.add(new Hex(relativeX, relativeY, true, false, false));
+    	}else if(typeH=="System1") {
+            this.hexes.add(new Hex(relativeX, relativeY, false, true, false));
+    	}else if(typeH=="System2") {
+            this.hexes.add(new Hex(relativeX, relativeY, false, false, true));
+    	}else {
+            this.hexes.add(new Hex(relativeX, relativeY, false, false, false));
+    	}
+    }
+    
+    /*public void addHex(int relativeX, int relativeY, String typeH) {
+    	if (typeH=="TriPrime") {
+    		this.hexes.add(new Hex(anchorX + relativeX, anchorY + relativeY, true, false, false));
+    	}else if(typeH=="System1") {
+            this.hexes.add(new Hex(anchorX + relativeX, anchorY + relativeY, false, true, false));
+    	}else if(typeH=="System2") {
+            this.hexes.add(new Hex(anchorX + relativeX, anchorY + relativeY, false, false, true));
+    	}else {
+            this.hexes.add(new Hex(anchorX + relativeX, anchorY + relativeY, false, false, false));
+    	}
+    }
+	*/
+    
+    @Override
+    public String toString() {
+    	StringBuilder sb = new StringBuilder();
+    	sb.append("SectorCard : ").append(name).append('\n');
+    	sb.append("Type: ").append(type).append("\n");
+        sb.append("Hexes:\n");
+
+        for (Hex hex : hexes) {
+            sb.append(" - ").append(hex).append("\n");
+        }
+        return sb.toString();
+    }
+    
+    public void createMiddleExteriorCard() {
+        String typeHex = "regularHex";
+        int[][] coordSystems = Helper.placeSystems("MiddleExteriorCard");
+        for (int i=0; i<3; i++) {
+        	for (int j=0; j<3; j++) {
+            	/*if ((i==0) && (j==2) || (i==2) && (j==2)) {
+            		typeHex = "regularHex";
+            	}*/
+        		if ( (i==1) && (j==2) ) {
+            		break;
+            	}
+            	if ((coordSystems[0][0]==i && coordSystems[0][1]==j) || (coordSystems[1][0]==i && coordSystems[1][1]==j)) {
+            		typeHex = "System1";
+            	}else if (coordSystems[2][0]==i && coordSystems[2][1]==j) {
+            		typeHex = "System2";
+            	}else {
+            		typeHex="RegularHex";
+            	}
+        		addHexInSector(i, j, typeHex);
+        	} 
+        }
+    }
+    
+    public void createCentralCard() {
+        for (int i=0; i<3; i++) {
+        	for (int j=0; j<3; j++) {
+            	if ((i==1) && (j==2)) {
+            		break;
+            	}if (i==1 ||(i==0 && j==1)||(i==2 && j==1)) {
+            		addHexInSector(i, j, "TriPrime");
+            	}else {
+            		addHexInSector(i, j, "regularHex");
+            	}
+        	}
+        }
+        this.estTriPrime = true;
+    }
+    
+    public void createExteriorCard() {
+        String typeHex = "regularHex";
+        int[][] coordSystems = Helper.placeSystems("ExteriorCard");
+        for (int i=0; i<3; i++) {
+        	for (int j=0; j<3; j++) {
+            	if ( ((i==0) || (i==2)) && (j==2) ) {
+            		break;
+            	}
+            	if ((coordSystems[0][0]==i && coordSystems[0][1]==j) || (coordSystems[1][0]==i && coordSystems[1][1]==j)) {
+            		typeHex = "System1";
+            	}else if (coordSystems[2][0]==i && coordSystems[2][1]==j) {
+            		typeHex = "System2";
+            	}else {
+            		typeHex="RegularHex";
+            	}
+        		addHexInSector(i, j, typeHex);
+        	} 
+        }
+    }
+
+    /*public static void main (String[] Args) {
+    	SectorCard carteMilieu = new SectorCard("CentralCard");
+    	SectorCard carteExt = new SectorCard("ExteriorCard");
+    	SectorCard carteMilExt = new SectorCard("MiddleExteriorCard");
+    	System.out.println(carteMilieu);
+    	System.out.println(carteExt);
+    	System.out.println(carteMilExt);	
+    }*/
+
+}

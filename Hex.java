bbb9fd434adcac1112ded2bbee31bb0f9a4a97ea
Speedy-yjo <anaellle.melo@gmail.com>
2024-12-10 11:13:14 +0100

    Update Hex.java

--- Hex.java
+++ Hex.java
@@ -7,15 +7,16 @@ import java.util.List;
  * La classe Hex permet de déterminer les différents Hex de la carte
  */
 public class Hex {
-	private double x_position;
-	private double y_position;
-
-	private List<Ship> shipsOnHex;
+	private int xPosition;
+	private int yPosition;
+	private ArrayList<Ship> shipsOnHex;
 	private int value;
 	private int nbMaxShips;
 	private boolean triPrime, systemHex1, systemHex2;
+	private boolean controlled=false;
+	private Player controlledBy=null;
 	
-	public Hex(double x, double y, boolean triPrime, boolean systemHex1, boolean systemHex2) {
+	public Hex(int x, int y, boolean triPrime, boolean systemHex1, boolean systemHex2) {
 		this.x_position=x;
 		this.y_position=y;
 		this.triPrime=triPrime;
@@ -31,6 +32,21 @@ public class Hex {
 		}else {
 			value=0;
 		}
+	}
+	public boolean isControlled() {
+		return controlled;
+	}
+
+	public void setControlled(boolean controlled) {
+		this.controlled = controlled;
+	}
+
+	public Player getControlledBy() {
+		return controlledBy;
+	}
+
+	public void setControlledBy(Player controlledBy) {
+		this.controlledBy = controlledBy;
 	}
 	public void setCoordinates(double x, double y) {
 		this.x_position = x; 

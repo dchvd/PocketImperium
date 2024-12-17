013b14adf762948f9341667d68bf6bf73ca869b9
Speedy-yjo <anaellle.melo@gmail.com>
2024-12-17 19:20:54 +0100

    Update Ship.java (rajout des positions initiales et de destroy ship et setPosition)

--- Ship.java
+++ Ship.java
@@ -1,25 +1,31 @@
 package pocket_imperium;
 
 public class Ship {
-	
+
 	//Attributs
 	private Color couleur;
 	private Player owner;
 	private int id;
 	private int xPosition;
 	private int yPosition;
-	
+
 	//Constructeur
+	// lorsque le ship n'est pas placé sur un hex, il est en x=-1 y=-1
 	public Ship( Player owner, int id) {
-		//this.couleur=couleur;
 		this.owner=owner;
 		this.id=id;
+		this.xPosition=-1;
+		this.yPosition=-1;
 		//this.couleur=owner.getCouleur();
 	}
-	
-	//Détruire le vaisseau
-	
-	
-	//set Position(x,y) 
-	//
+
+	public void setPosition(int x,int y){
+		this.xPosition=x;
+		this.yPosition=y;
+	}
+	public void destroyShip(){
+		this.xPosition=-1;
+		this.yPosition=-1;
+
+	}
 }

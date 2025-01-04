9854fafa0a25d5f237e1f1907b646c24efe9757e
dchvd <dachavde@gmail.com>
2025-01-04 19:29:52 +0100

    je pense pas avoir modifié mais au cas où

--- Hex.java
+++ Hex.java
@@ -15,14 +15,14 @@ public class Hex {
 	private boolean triPrime, systemHex1, systemHex2;
 	private boolean controlled=false;
 	private Player controlledBy=null;
-	
+
 	public Hex(int x, int y, boolean triPrime, boolean systemHex1, boolean systemHex2) {
 		this.xPosition=x;
 		this.yPosition=y;
 		this.triPrime=triPrime;
 		this.systemHex1=systemHex1;
 		this.systemHex2=systemHex2;
-		
+
 		if(triPrime==true) {
 			value = 3;
 			this.nbMaxShips=4;
@@ -37,7 +37,7 @@ public class Hex {
 			this.nbMaxShips=1; // potentiellement 0
 		}
 	}
-	
+
 
 	public boolean isControlled() {
 		return controlled;
@@ -56,21 +56,21 @@ public class Hex {
 		this.controlledBy = controlledBy;
 	}
 	public void setCoordinates(int x, int y) {
-		this.xPosition = x; 
+		this.xPosition = x;
 		this.yPosition = y;
 	}
-	
+
 	@Override
-    public String toString() {
-        /**return "Hex{" +
-                " x='" + xPosition + '\'' +
-                " y='" + yPosition + '\'' +
-                " value='" + value + '\'' +
-                " nb_vaisseaux='"+this.shipsOnHex.size()+'\''+
-                '}';**/
+	public String toString() {
+		/**return "Hex{" +
+		 " x='" + xPosition + '\'' +
+		 " y='" + yPosition + '\'' +
+		 " value='" + value + '\'' +
+		 " nb_vaisseaux='"+this.shipsOnHex.size()+'\''+
+		 '}';**/
 		String shipsInString = "*".repeat(this.shipsOnHex.size());
 		return " { x=" + xPosition + "y=" + yPosition + "| systLevel=" + value + "ships="+ shipsInString +"}";
-    }
+	}
 
 	public int getxPosition() {
 		return xPosition;
@@ -80,7 +80,7 @@ public class Hex {
 		return yPosition;
 	}
 
-	
+
 	public ArrayList<Ship> getShipsOnHex() {
 		return shipsOnHex;
 	}
@@ -90,22 +90,22 @@ public class Hex {
 	public int getValue() {
 		return value;
 	}
-	
+
 	public int getNbMaxShips() {
 		return nbMaxShips;
 	}
-	
+
 	public boolean isTriPrime() {
 		return triPrime;
 	}
-	
+
 	public boolean isSystemHex1() {
 		return systemHex1;
 	}
-	
+
 	public boolean isSystemHex2() {
 		return systemHex2;
 	}
-	
-	
+
+
 }

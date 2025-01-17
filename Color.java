5c7233c7dc8d23742df2edb3eefc2b7a63436b88
Speedy-yjo <anaellle.melo@gmail.com>
2025-01-17 19:36:05 +0100

    Update Color.java

--- Color.java
+++ Color.java
@@ -4,6 +4,9 @@ package pocket_imperium;
  * Enumération représentant les couleurs disponibles pour les joueurs
  * dans le jeu Pocket Imperium.
  * Chaque couleur est unique et peut être attribuée à un joueur au début du jeu.
+ * 
+ * @author Anaelle Melo, Daria Avdeeva
+ * @version 1.0
  */
 public enum Color {
 	JAUNE,

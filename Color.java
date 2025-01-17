8a5f3bb40f5d9551fab73721103d2a33de3a9198
dchvd <dachavde@gmail.com>
2025-01-17 18:09:14 +0100

    javadoc

--- Color.java
+++ Color.java
@@ -1,5 +1,10 @@
 package pocket_imperium;
 
+/**
+ * Enumération représentant les couleurs disponibles pour les joueurs
+ * dans le jeu Pocket Imperium.
+ * Chaque couleur est unique et peut être attribuée à un joueur au début du jeu.
+ */
 public enum Color {
 	JAUNE,
 	ROUGE,

223251c718fdc7d082ca24b53a7a942f9188e0f1
Speedy-yjo <anaellle.melo@gmail.com>
2025-01-17 19:35:39 +0100

    Update GameStrategy.java

--- GameStrategy.java
+++ GameStrategy.java
@@ -3,6 +3,7 @@ package pocket_imperium;
 /**
  * Enumération représentant les différentes stratégies de jeu disponibles pour les joueurs virtuels. 
  * Chaque stratégie définit une approche distincte que le joueur peut adopter.
+ * 
  * @author Anaelle Melo, Daria Avdeeva
  * @version 1.0
  */

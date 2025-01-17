af6d4c8ebcb6083b8c8ff4281b44ea0a7fa2e83e
dchvd <dachavde@gmail.com>
2025-01-17 18:11:49 +0100

    javadoc

--- GameStrategy.java
+++ GameStrategy.java
@@ -1,9 +1,11 @@
 package pocket_imperium;
 
+/**
+ * Enumération représentant les différentes stratégies de jeu disponibles pour les joueurs virtuels. 
+ * Chaque stratégie définit une approche distincte que le joueur peut adopter.
+ */
 public enum GameStrategy {
 	AGGRESSIVE,
 	EXPLORER,
 	EXPENDER;
-
-
 }
